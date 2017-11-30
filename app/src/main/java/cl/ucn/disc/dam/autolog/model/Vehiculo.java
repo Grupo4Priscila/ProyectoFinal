35e1ba5f67b78dc8f34d09409c16998ee5a5a33d
Raos <raos.fast@gmail.com>
2017-11-30 17:32:47 -0300

    Se agrego la clase vehiculo y de verdad libreria lombok

--- app/src/main/java/cl/ucn/disc/dam/autolog/model/Vehiculo.java
+++ app/src/main/java/cl/ucn/disc/dam/autolog/model/Vehiculo.java
@@ -2,6 +2,7 @@ package cl.ucn.disc.dam.autolog.model;
 
 
 import lombok.Builder;
+import lombok.Getter;
 
 /**
  * Created by RaosF on 30-11-2017.
@@ -14,19 +15,24 @@ public final class Vehiculo {
     /**
      * Persona responsable;
      */
-
+    @Getter
     String patente;
 
+    @Getter
     String marca;
 
+    @Getter
     String color;
 
+    @Getter
     String modelo;
 
+    @Getter
     int ano;
 
+    @Getter
     String descripcion;
-
+    
 
 
 

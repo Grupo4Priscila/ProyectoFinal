02d305c255e72224e9da952a4cdf5445360a208e
Raos <raos.fast@gmail.com>
2017-11-30 17:17:11 -0300

    Se agrego la clase vehiculo y la libreria lombok

---
+++ app/src/main/java/cl/ucn/disc/dam/autolog/model/Vehiculo.java
@@ -0,0 +1,30 @@
+package cl.ucn.disc.dam.autolog.model;
+
+
+/**
+ * Created by RaosF on 30-11-2017.
+ */
+
+
+public final class Vehiculo {
+
+    Persona responsable;
+
+    String patente;
+
+    String marca;
+
+    String color;
+
+    String modelo;
+
+    int ano;
+
+    String descripcion;
+
+
+
+
+}
+
+
